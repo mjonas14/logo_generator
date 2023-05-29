19d7daeaa46668f40e420dc64a8632e5a3d58b50
Marcus Jonas <marcusjonas@Marcuss-MacBook-Pro.local>
2023-05-29 14:57:24 -0700

    Completed svg logo text to generate successfully and correctly. Removed console logs. Added tests and ensured they worked

--- index.js
+++ index.js
@@ -9,15 +9,18 @@ const questions = [
   "What colour do you want the shape to be?",
 ];
 
-function writeToFile(fileName, data) {
-  const content = generateMarkdown(data);
-  fs.writeFile(fileName, content, (err) => {
-    if (err) {
-      return console.log(err);
-    }
+function isValidText(input) {
+  if (input.length <= 3 && input.length >=1) return true;
 
-    console.log("Success!");
-  });
+  return 'Please input text between 1 and 3 characters';
+};
+
+function isValidColour(input) {
+  let validColours = ['aliceblue', 'antiquewhite', 'aqua', 'aquamarine', 'azure', 'beige', 'bisque', 'black', 'blanchedalmond', 'blue', 'blueviolet', 'brown', 'burlywood', 'cadetblue', 'chartreuse', 'chocolate', 'coral', 'cornflowerblue', 'cornsilk', 'crimson', 'cyan', 'darkblue', 'darkcyan', 'darkgoldenrod', 'darkgray', 'darkgrey', 'darkgreen', 'darkkhaki', 'darkmagenta', 'darkolivegreen', 'darkorange', 'darkorchid', 'darkred', 'darksalmon', 'darkseagreen', 'darkslateblue', 'darkslategray', 'darkslategrey', 'darkturquoise', 'darkviolet', 'deeppink', 'deepskyblue', 'dimgray', 'dimgrey', 'dodgerblue', 'firebrick', 'floralwhite', 'forestgreen', 'fuchsia', 'gainsboro', 'ghostwhite', 'gold', 'goldenrod', 'gray', 'grey', 'green', 'greenyellow', 'honeydew', 'hotpink', 'indianred', 'indigo', 'ivory', 'khaki', 'lavender', 'lavenderblush', 'lawngreen', 'lemonchiffon', 'lightblue', 'lightcoral', 'lightcyan', 'lightgoldenrodyellow', 'lightgray', 'lightgrey', 'lightgreen', 'lightpink', 'lightsalmon', 'lightseagreen', 'lightskyblue', 'lightslategray', 'lightslategrey', 'lightsteelblue', 'lightyellow', 'lime', 'limegreen', 'linen', 'magenta', 'maroon', 'mediumaquamarine', 'mediumblue', 'mediumorchid', 'mediumpurple', 'mediumseagreen', 'mediumslateblue', 'mediumspringgreen', 'mediumturquoise', 'mediumvioletred', 'midnightblue', 'mintcream', 'mistyrose', 'moccasin', 'navajowhite', 'navy', 'oldlace', 'olive', 'olivedrab', 'orange', 'orangered', 'orchid', 'palegoldenrod', 'palegreen', 'paleturquoise', 'palevioletred', 'papayawhip', 'peachpuff', 'peru', 'pink', 'plum', 'powderblue', 'purple', 'red', 'rosybrown', 'royalblue', 'saddlebrown', 'salmon', 'sandybrown', 'seagreen', 'seashell', 'sienna', 'silver', 'skyblue', 'slateblue', 'slategray', 'slategrey', 'snow', 'springgreen', 'steelblue', 'tan', 'teal', 'thistle', 'tomato', 'turquoise', 'violet', 'wheat', 'white', 'whitesmoke', 'yellow', 'yellowgreen'];
+
+  if (validColours.includes(input.toLowerCase()) || /^#[0-9A-F]{6}$/i.test(input.toLowerCase())) return true;
+
+  return 'Please input a valid colour';
 };
 
 function makeLogo(data) {
@@ -40,27 +43,29 @@ function init() {
         name: "text",
         message: questions[0],
         type: "input",
+        validate: isValidText,
       },
       {
         name: "text_colour",
         message: questions[1],
         type: "input",
+        validate: isValidColour,
       },
       {
         name: "shape",
         message: questions[2],
         type: "rawlist",
-        choices: ["Traingle", "Circle", "Square"],
+        choices: ["Triangle", "Circle", "Square"],
       },
       {
         name: "shape_colour",
         message: questions[3],
         type: "input",
+        validate: isValidColour,
       },
     ])
 
     .then((answers) => {
-      console.log('Test 1', answers);
       makeLogo(answers);
     });
 }
